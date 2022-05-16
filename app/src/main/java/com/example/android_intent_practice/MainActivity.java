a386404f22ab0dead531105f86449b622a47754d
Talha Masood <talha8665i@gmail.com>
2022-05-16 17:23:36 +0500

    Dialer Activity Created

--- app/src/main/java/com/example/android_intent_practice/MainActivity.java
+++ app/src/main/java/com/example/android_intent_practice/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.android_intent_practice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void Dial(View view){
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:+923040169808"));
+
+            startActivity(intent);
+
+
     }
 }
